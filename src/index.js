2d451c9d08e8ec367ab806adcdad9ead4ba07aa6
nyakiri_margaret <margaret.karanja@student.moringaschool.com>
2022-12-05 11:17:15 +0300

    Complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskList = new Tasklist();
+
+  const newTaskForm =document.getElementById("create-task-form");
+  const newTaskDescription =document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+
+  //additional tasks
+  const newtaskUl =document.getElementById("tasks");
+
+
+  //addEventListener
+
+  newTaskForm.addEventListener("submit", createNewTask);
 });
+  const createNewTask = e =>{
+    e.preventDefault();
+    const newTaskDescription =document.getElementById("new-task-description");
+      const newTask = document.createElement("li");
+    newTask.innerText= newTaskDescription.value;
+
+    appendNewTask(newTask);
+    e.target.reset();
+    
+  };
+
+  const appendNewTask = task =>{
+    document.getElementById("task").appendChild(task);};
+  
